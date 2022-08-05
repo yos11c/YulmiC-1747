d5bd5083f51372bd095b757725545785c7728a87
yos11c <ycontrerasc3@miumg.edu.gt>
2022-08-05 18:28:12 +0000

    servlet para los response

---
+++ NewServlet.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import Clase.ClienteModell;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Yosmeri C. Cruz
+ */
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class NewServlet extends HttpServlet {
+    ClienteModell ClienteModell;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            ClienteModell= new ClienteModell();
+            ClienteModell.setNombre(request.getParameter("nombre"));
+             ClienteModell.setApellido(request.getParameter("apellido"));
+             ClienteModell.setCodigo(request.getParameter("Codigo"));
+             ClienteModell.setNumero(request.getParameter("numero"));
+             ClienteModell.setCorreo(request.getParameter("correo"));
+        out.print("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css\" integrity=\"sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N\" crossorigin=\"anonymous\">\n" +
+"          <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct\" crossorigin=\"anonymous\"></script>");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>ClienteModell</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.print("<div class=\"container\">");
+          out.println("<h1> CLIENTES  " + "</h1>");
+            out.println("<h3>"  +"Nombre " + ClienteModell.getNombre() + "</h3>");
+            out.println("<h3>"  +"Codigo " + ClienteModell.getCodigo()+ "</h3>");
+            out.println("<h3>"  +"Apellido: " + ClienteModell.getApellido()+ "</h3>");
+             out.println("<h3>" +"Numero: " + ClienteModell.getNumero()+ "</h3>");
+              out.println("<h3>" +"Correo: " + ClienteModell.getCorreo()+ "</h3>");
+             out.print("<a class=\"btn btn-primary\" href=\"index.html\" role=\"button\">Registro De Clientes</a>");
+             out.print("   </div>");
+             out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
